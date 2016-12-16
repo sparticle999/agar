1883bf05b9e1b432425b3135ce35b18b983ddfab
Sparticle999 <alexscruton@gmail.com>
2016-12-16 16:15:25 +0000

    update

--- sketch.js
+++ sketch.js
@@ -4,18 +4,18 @@ var zoom = 1;
 
 function setup(){
 	createCanvas(600,600);
-	blob = new Blob(0, 0, 64);
-	for(var i = 0; i < 200; i++){
-		var x = random(-width, width);
-		var y = random(-height, height);
-		blobs[i] = new Blob(x, y, 16);
+	blob = new Blob(0, 0, 16);
+	for(var i = 0; i < 2000; i++){
+		var x = random(-5*width, 5*width);
+		var y = random(-5*height, 5*height);
+		blobs[i] = new Blob(x, y, 4);
 	}
 }
 
 function draw(){
 	background(0)
 	translate(width/2, height/2)
-	var newzoom = 64/blob.r;
+	var newzoom = 32/blob.r;
 	zoom = lerp(zoom, newzoom, 0.1);
 	scale(zoom);
 	translate(-blob.pos.x, -blob.pos.y)
@@ -24,9 +24,9 @@ function draw(){
 		blobs[i].show();
 		if(blob.eats(blobs[i])){
 			//blobs.splice(i, 1);
-			var x = random(-width, width);
-			var y = random(-height, height);
-			blobs[i] = new Blob(x, y, 16);
+			var x = random(-5*width, 5*width);
+			var y = random(-5*height, 5*height);
+			blobs[i] = new Blob(x, y, 4);
 		}
 	}
 
